b4ec178fa431e7003785765d32fe37690e4f34ec
Adhiseem Pandey <adhiseempandey@gmail.com>
2025-04-01 00:05:06 +0530

    Adhiseem

---
+++ Kotlin_Heros_Practice_12/a.cpp
@@ -0,0 +1,79 @@
+
+/**********************KOTLIN CODE *************************/
+/*
+import java.util.*
+
+fun main() {
+    val scanner = Scanner(System.`in`)
+    val t = scanner.nextInt()
+    
+    repeat(t) {
+        val n = scanner.nextInt()
+        val a = n - 1
+        println(a)
+        // println(if (solve(n, b)) "YES" else "NO")
+    }
+}
+*/
+
+
+
+
+/**********************CPP Code******************/
+
+/*
+#include <bits/stdc++.h>
+// Look for limits
+#define ll long long
+#define vi vector<int>
+#define vll vector<ll>
+// check on the return statement 
+// #define int long long 
+#define inint(x) int x; cin >> x;
+#define st string
+#define I int
+#define foreach(i, j, k, in) for(int i = j; i < k; i += in) // loop 
+#define rep(i, j) foreach(i, 0, j, 1)
+#define adhi(x) ((int)(x).size()) // Macro to get the size of a vector
+using namespace std;
+
+// void solve(){
+//     ll n ; cin >> n ; 
+//     ll m ; cin >> m ;
+//     vi x(n) , y(n) ;
+//     for( ll i = 0 ; i < n ; i++){
+//         cin >> x[i] >> y[i] ;
+//     }
+// }
+
+
+// void solve(){
+
+// }
+
+// int solve(a,s){
+//
+// }
+
+// bool solve(ll n, const vi &b) { 
+//     foreach(i, 0, adhi(), 1) {
+//         }
+// }
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int t;
+    cin >> t;
+    while (t--) {
+        inint(n);
+        I a = n - 1 ;
+        cout << a << "\n";
+       // cout << (solve(n, b) ? "YES\n" : "NO\n");
+    }
+
+    return 0;
+}
+*/
